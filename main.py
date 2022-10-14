591f6aea9f88ae1ef58363b9865093e8f7f64228
Sabir <qwartopl@mail.ru>
2022-10-14 18:57:12 +0300

    Add new function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def print_hi(name):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+    def foo():
+        print("Foo!")
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if __name__ == '__main__':
+        print_hi('PyCharm')
